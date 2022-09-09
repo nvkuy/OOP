a231948c1fecc0836dbcb2291bad66f393e62c42
nvkuy <nguyenvukhanhuy@gmail.com>
2022-09-09 07:12:41 +0700

    Simplify code BT2

--- BT2/main.cpp
+++ BT2/main.cpp
@@ -6,13 +6,12 @@ struct date {
     int d, m, y;
     date() {
     }
-    date(int dd, int mm, int yy) {
-        d = dd, m = mm, y = yy;
+    date(int d, int m, int y) {
+        this->d = d, this->m = m, this->y = y;
     }
 };
 
-date operator ++ (date &cur, int) {
-    date bef = cur;
+void next_date(date &cur) {
     int dayOfMonth[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
     if (cur.y % 4 == 0)
         dayOfMonth[1]++;
@@ -25,52 +24,39 @@ date operator ++ (date &cur, int) {
             cur.y++;
         }
     }
+}
+
+void prev_date(date &cur) {
+    int dayOfMonth[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    if (cur.y % 4 == 0)
+        dayOfMonth[1]++;
+    cur.d--;
+    if (cur.d < 1) {
+        cur.m--;
+        if (cur.m < 1) {
+            cur.m = 12;
+            cur.y--;
+        }
+        cur.d = dayOfMonth[cur.m - 1];
+    }
+}
+
+date operator ++ (date &cur, int) {
+    date bef = cur;
+    next_date(cur);
     return bef;
 }
 date & operator ++ (date &cur) {
-    int dayOfMonth[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-    if (cur.y % 4 == 0)
-        dayOfMonth[1]++;
-    cur.d++;
-    if (cur.d > dayOfMonth[cur.m - 1]) {
-        cur.d = 1;
-        cur.m++;
-        if (cur.m > 12) {
-            cur.m = 1;
-            cur.y++;
-        }
-    }
+    next_date(cur);
     return cur;
 }
 date operator -- (date &cur, int) {
     date bef = cur;
-    int dayOfMonth[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-    if (cur.y % 4 == 0)
-        dayOfMonth[1]++;
-    cur.d--;
-    if (cur.d < 1) {
-        cur.m--;
-        if (cur.m < 1) {
-            cur.m = 12;
-            cur.y--;
-        }
-        cur.d = dayOfMonth[cur.m - 1];
-    }
+    prev_date(cur);
     return bef;
 }
 date & operator -- (date &cur) {
-    int dayOfMonth[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-    if (cur.y % 4 == 0)
-        dayOfMonth[1]++;
-    cur.d--;
-    if (cur.d < 1) {
-        cur.m--;
-        if (cur.m < 1) {
-            cur.m = 12;
-            cur.y--;
-        }
-        cur.d = dayOfMonth[cur.m - 1];
-    }
+    prev_date(cur);
     return cur;
 }
 istream & operator >> (istream &in, date &d) {
@@ -106,7 +92,7 @@ int main()
     cin >> dt;
     --dt;
     cout << dt << endl;
-    ++dt;
+    dt++;
     cout << dt;
 
     return 0;
